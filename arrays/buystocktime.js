d5aa353d3874e95254873ec1aafec3bdd9e8ecf4
Ubuntu <navs6164@gmail.com>
2024-12-16 19:55:21 +0530

    arrays two problems

---
+++ arrays/buystocktime.js
@@ -0,0 +1,18 @@
+//? first try
+
+// var maxProfit = function(prices) {
+//     let maxPro = 0;
+//     for(let i = 0; i < prices.length; i++) {
+//         for(let j = i+1; j < prices.length; j++){
+//             if(prices[j] - prices[i] > maxPro){
+//                 maxPro = prices[j] - prices[i];
+//             }
+//         }
+//     }
+//     // return maxPro;
+//     console.log(maxPro);
+    
+// };
+
+// prices = [7,1,5,3,6,4];
+// maxProfit(prices);
