a30fed5fb03667deb1dde998a1bfc080d224ddff
Ubuntu <navs6164@gmail.com>
2024-12-17 13:21:52 +0530

    buy sell

--- arrays/buystocktime.js
+++ arrays/buystocktime.js
@@ -14,11 +14,25 @@
     
 // };
 
-// prices = [7,1,5,3,6,4];
-// maxProfit(prices);
 
 //? second try by own
 
 //! no mind can search for algorithm 
 
 //? search the algorthm and then i hear about kadane's name 
+
+var maxProfit = function(prices) {
+    let maxPro = 0;
+    let lowest = prices[0];
+    for( let i = 0; i < prices.length; i++ ) {
+        lowest = Math.min(lowest, prices[i]);
+        let profit = prices[i] - lowest;
+        maxPro = Math.max(profit, maxPro);
+    }
+    
+    console.log(maxPro);    
+    
+};
+
+prices = [7,1,5,3,6,4];
+maxProfit(prices);
